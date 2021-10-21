962c07a4d3ed6ab9211a06dd93391a7b737fc89b
Zachary Stence <zachstence@gmail.com>
2021-10-21 18:38:14 -0500

    feat: basic rendering of Card suits/ranks

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import {Card, Rank, Suit} from "../../Deck/Deck";
+interface CardCompProps {
+    card: Card;
+}
 
-const Card: React.FC = () => {
-    return <div>Card</div>;
+const CardComp: React.FC<CardCompProps> = (props: CardCompProps) => {
+    const suitToChar = (suit: Suit) => {
+        switch (suit) {
+            case "spades": return "♤";
+            case "hearts": return "♡";
+            case "diamonds": return "♢";
+            case "clubs": return "♧";
+        }
+    }
+
+    const rankToChar = (rank: Rank) => {
+        switch (rank) {
+            case 1:
+                return "A";
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                return rank.toString();
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+        }
+    }
+
+    return (
+        <div className="card">
+            {rankToChar(props.card.rank)}
+            {suitToChar(props.card.suit)}
+        </div>
+    );
 };
 
-export default Card;
+export default CardComp;
